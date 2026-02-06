9d263d1afaa1b075113bdaa7109e955145f5af72
junjunzhou <junjunzhou@sohu-inc.com>
2026-02-06 14:59:00 +0800

    upd

--- webUtil/encoding.go
+++ webUtil/encoding.go
@@ -1,38 +1,38 @@
 package webUtil
 
 import (
-    "compress/gzip"
-    "github.com/ZhouJunjun/goLib/log4j"
-    "net/http"
+	"compress/gzip"
+	"github.com/ZhouJunjun/goLib/log4j"
+	"net/http"
 )
 
-func NewGZipWriter(writer http.ResponseWriter) http.ResponseWriter {
-    writer.Header().Set("Content-Encoding", "gzip")
-    log4j.Info("new gzip writer")
-    return &gzipWriter{
-        originWriter: writer,
-        gzipWriter:   gzip.NewWriter(writer),
-    }
+func NewGZipWriter(writer http.ResponseWriter) *gzipWriter {
+	writer.Header().Set("Content-Encoding", "gzip")
+	log4j.Info("new gzip writer")
+	return &gzipWriter{
+		originWriter: writer,
+		gzipWriter:   gzip.NewWriter(writer),
+	}
 }
 
 type gzipWriter struct {
-    originWriter http.ResponseWriter
-    gzipWriter   *gzip.Writer
+	originWriter http.ResponseWriter
+	gzipWriter   *gzip.Writer
 }
 
 func (p *gzipWriter) Header() http.Header {
-    return p.originWriter.Header()
+	return p.originWriter.Header()
 }
 
 func (p *gzipWriter) Write(data []byte) (int, error) {
-    return p.gzipWriter.Write(data)
+	return p.gzipWriter.Write(data)
 }
 
 func (p *gzipWriter) WriteHeader(statusCode int) {
-    p.originWriter.WriteHeader(statusCode)
+	p.originWriter.WriteHeader(statusCode)
 }
 
 func (p *gzipWriter) Close() error {
-    log4j.Info("close gzip writer")
-    return p.gzipWriter.Close()
+	log4j.Info("close gzip writer")
+	return p.gzipWriter.Close()
 }
